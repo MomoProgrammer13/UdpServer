efbb2e7037d096693bba8dc1b068c8c8007c5bb3
gabriel <gabrielfanha@gmail.com>
2024-10-19 00:48:43 -0300

    Modificando

--- main.go
+++ main.go
@@ -1,36 +1,94 @@
 package main
 
 import (
+	"encoding/binary"
+	"encoding/hex"
 	"fmt"
+	"log"
 	"net"
+	"os"
+	"time"
 )
 
-func sendResponse(conn *net.UDPConn, addr *net.UDPAddr) {
-	_, err := conn.WriteToUDP([]byte("From server: Hello I got your message "), addr)
+const (
+	HOST = "127.0.0.1"
+	PORT = 9001
+	TYPE = "udp"
+)
+
+func handleIncomingRequests(conn *net.UDPConn, addr *net.UDPAddr) {
+	println("Received a request: " + addr.String())
+	headerBuffer := make([]byte, 1024)
+
+	_, _, err := conn.ReadFromUDP(headerBuffer)
 	if err != nil {
-		fmt.Printf("Couldn't send response %v", err)
+		log.Fatal(err)
 	}
+
+	var name string
+	var reps uint32
+
+	if headerBuffer[0] == byte(1) && headerBuffer[1023] == byte(0) {
+		reps = binary.BigEndian.Uint32(headerBuffer[1:5])
+		lengthOfName := binary.BigEndian.Uint32(headerBuffer[5:9])
+		name = string(headerBuffer[9 : 9+lengthOfName])
+	} else {
+		log.Fatal("Invalid header")
+	}
+
+	conn.WriteToUDP([]byte("Header Received"), addr)
+
+	dataBuffer := make([]byte, 1024)
+
+	file, err := os.Create("./received/" + name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < int(reps); i++ {
+		_, _, err := conn.ReadFromUDP(dataBuffer)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if dataBuffer[0] == byte(0) && dataBuffer[1023] == byte(1) {
+			segmentNumber := dataBuffer[1:5]
+			fmt.Printf("Segment Number: %d\n", binary.BigEndian.Uint32(segmentNumber))
+			length := binary.BigEndian.Uint32(dataBuffer[5:9])
+			fmt.Printf("File Data: %s\n", hex.EncodeToString(dataBuffer[9:9+length]))
+			file.Write(dataBuffer[9 : 9+length])
+		} else {
+			log.Fatal("Invalid Segment")
+		}
+
+		conn.WriteToUDP([]byte("Segment Received"), addr)
+	}
+
+	time := time.Now().UTC().Format("Monday, 02-Jan-06 15:04:05 MST")
+	conn.WriteToUDP([]byte(time), addr)
+
+	file.Close()
 }
 
 func main() {
-	p := make([]byte, 2048)
 	addr := net.UDPAddr{
-		Port: 1234,
-		IP:   net.ParseIP("127.0.0.1"),
+		Port: PORT,
+		IP:   net.ParseIP(HOST),
 	}
-	ser, err := net.ListenUDP("udp", &addr)
+	listen, err := net.ListenUDP(TYPE, &addr)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+	defer listen.Close()
+
+	println("Server has started on PORT " + fmt.Sprint(PORT))
 
 	for {
-		_, remoteaddr, err := ser.ReadFromUDP(p)
-		fmt.Printf("Read a message from %v %s \n", remoteaddr, p)
+		buffer := make([]byte, 1024)
+		_, clientAddr, err := listen.ReadFromUDP(buffer)
 		if err != nil {
-			fmt.Printf("Some error %v", err)
-			continue
+			log.Fatal(err)
 		}
-		go sendResponse(ser, remoteaddr)
+		go handleIncomingRequests(listen, clientAddr)
 	}
-
 }
