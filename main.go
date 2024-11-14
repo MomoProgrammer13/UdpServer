85c03a44a70bbbd40cff17242922abe87887be69
gabriel <gabrielfanha@gmail.com>
2024-11-13 21:19:13 -0300

    ebaaa

--- main.go
+++ main.go
@@ -16,14 +16,25 @@ const (
 	TYPE = "udp"
 )
 
-type MetaData struct {
+type ResponseMetaData struct {
 	Name     string
 	FileSize int64
-	Reps     int32
+	Reps     uint32
 	Data     []byte
 }
 
-func (meta MetaData) metaDataToBytes() []byte {
+type Packet struct {
+	Reps uint32
+	Data []byte
+}
+
+type RequestMetaData struct {
+	Name string
+	Reps uint32
+	Miss bool
+}
+
+func (meta ResponseMetaData) ResponseMetaDataToBytes() []byte {
 	var metaBytes bytes.Buffer
 	enc := gob.NewEncoder(&metaBytes)
 
@@ -35,14 +46,41 @@ func (meta MetaData) metaDataToBytes() []byte {
 	return metaBytes.Bytes()
 }
 
+func (meta RequestMetaData) RequestMetaDataToBytes() []byte {
+	var metaBytes bytes.Buffer
+	enc := gob.NewEncoder(&metaBytes)
+
+	err := enc.Encode(meta)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return metaBytes.Bytes()
+}
+
+func (meta Packet) PacketToBytes() []byte {
+	var metaBytes bytes.Buffer
+	enc := gob.NewEncoder(&metaBytes)
+
+	err := enc.Encode(meta)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return metaBytes.Bytes()
+}
+
 func handleIncomingRequests(conn *net.UDPConn, addr *net.UDPAddr, buffer []byte) {
 	println("Received a request: " + addr.String())
 
-	//var name string
-	//var reps uint32
-
 	fmt.Printf("Received: %s\n", buffer)
-	file, err := os.OpenFile(`LICENSE`, os.O_RDONLY, 0755)
+	dec := gob.NewDecoder(bytes.NewReader(buffer))
+	var request RequestMetaData
+	// TODO - Check if the header is valid
+	errorDecode := dec.Decode(&request)
+	if errorDecode != nil {
+		log.Fatal(errorDecode)
+	}
+	// TODO - Check if the file exist
+	file, err := os.OpenFile(request.Name, os.O_RDONLY, 0755)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -50,101 +88,71 @@ func handleIncomingRequests(conn *net.UDPConn, addr *net.UDPAddr, buffer []byte)
 
 	fi, err := file.Stat()
 	if err != nil {
-		// Could not obtain stat, handle error
+		log.Fatal(err)
 	}
 
 	fmt.Printf("File Size: %d\n", fi.Size())
-
-	//if buffer[0] == byte(1) && buffer[1023] == byte(0) {
-	//	reps = binary.BigEndian.Uint32(buffer[1:5])
-	//	lengthOfName := binary.BigEndian.Uint32(buffer[5:9])
-	//	name = string(buffer[9 : 9+lengthOfName])
-	//} else {
-	//	log.Fatal("Invalid header")
-	//}
-
 	fileBuffer := make([]byte, fi.Size())
-
 	_, errorBuffer := file.Read(fileBuffer)
 	if errorBuffer != nil {
 		return
 	}
-
-	headerSize := 124
-
+	headerSize := 64
 	bodySize := int64(1024 - headerSize)
-
 	fmt.Printf("Header Size: %d\n", headerSize)
 	fmt.Printf("Body Size: %d\n", bodySize)
-
 	quantidadeDeReps := fi.Size() / bodySize
 
-	_, err = conn.WriteToUDP([]byte("Parts: "+fmt.Sprint(quantidadeDeReps)), addr)
-	if err != nil {
-		log.Fatal(err)
-	}
+	if request.Miss {
 
-	time.Sleep(1 * time.Second)
+	} else {
+		if request.Reps == 0 {
+			fileInformation := ResponseMetaData{
+				Name:     fi.Name(),
+				FileSize: fi.Size(),
+				Reps:     uint32(quantidadeDeReps),
+				Data:     []byte(""),
+			}.ResponseMetaDataToBytes()
+			_, err = conn.WriteToUDP(fileInformation, addr)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-	for i := int64(0); i <= quantidadeDeReps; i++ {
-
-		var dataBuffer []byte
-		fmt.Printf("Reps: %d\n", quantidadeDeReps-i)
-		fmt.Printf("Body Size: %v", i*bodySize)
-		if size := fi.Size() - i*bodySize; size < bodySize {
-			dataBuffer = fileBuffer[i*bodySize : fi.Size()]
-			fmt.Printf(" Body Size: %v", len(dataBuffer))
-		} else {
-			dataBuffer = fileBuffer[i*bodySize : i*bodySize+bodySize]
-			fmt.Printf(" Body Size: %v", len(dataBuffer))
+			time.Sleep(1 * time.Second)
 		}
 
-		responseBuffer := MetaData{
-			Name:     fi.Name(),
-			FileSize: fi.Size(),
-			Reps:     int32(quantidadeDeReps - i),
-			Data:     dataBuffer,
-		}.metaDataToBytes()
-		conn.WriteToUDP(responseBuffer, addr)
+		quantidadeEnvio := func() int64 {
+			if request.Reps == 0 {
+				if fi.Size() < bodySize {
+					return 0
+				} else if fi.Size() < bodySize*10 {
+					return quantidadeDeReps
+				}
+				return 9
+			}
+			if uint32(quantidadeDeReps)/(request.Reps) > 0 {
+				return 9
+			}
+			return quantidadeDeReps % int64(request.Reps)
+		}
+		for i := int64(request.Reps); i <= quantidadeEnvio(); i++ {
+			var dataBuffer []byte
+			fmt.Printf("Reps: %d", quantidadeDeReps-i)
+			fmt.Printf(" Size sended: %v", i*bodySize)
+			if size := fi.Size() - i*bodySize; size < bodySize {
+				dataBuffer = fileBuffer[i*bodySize : fi.Size()]
+				fmt.Printf(" Body Size: %v\n", len(dataBuffer))
+			} else {
+				dataBuffer = fileBuffer[i*bodySize : i*bodySize+bodySize]
+				fmt.Printf(" Body Size: %v\n", len(dataBuffer))
+			}
+			packetBuffer := Packet{
+				Reps: uint32(quantidadeDeReps - i),
+				Data: dataBuffer,
+			}.PacketToBytes()
+			conn.WriteToUDP(packetBuffer, addr)
+		}
 	}
-
-	//dataBuffer := MetaData{
-	//	Name:     "calendar-clock.svg",
-	//	FileSize: fi.Size(),
-	//	Reps:     1,
-	//	Data:     fileBuffer,
-	//}.metaDataToBytes()
-	//
-	//conn.WriteToUDP(dataBuffer, addr)
-
-	//file, err := os.Create("./received/" + name)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
-	//for i := 0; i < int(reps); i++ {
-	//	_, _, err := conn.ReadFromUDP(dataBuffer)
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
-	//
-	//	if dataBuffer[0] == byte(0) && dataBuffer[1023] == byte(1) {
-	//		segmentNumber := dataBuffer[1:5]
-	//		fmt.Printf("Segment Number: %d\n", binary.BigEndian.Uint32(segmentNumber))
-	//		length := binary.BigEndian.Uint32(dataBuffer[5:9])
-	//		fmt.Printf("File Data: %s\n", hex.EncodeToString(dataBuffer[9:9+length]))
-	//		file.Write(dataBuffer[9 : 9+length])
-	//	} else {
-	//		log.Fatal("Invalid Segment")
-	//	}
-	//
-	//	conn.WriteToUDP([]byte("Segment Received"), addr)
-	//}
-
-	//time := time.Now().UTC().Format("Monday, 02-Jan-06 15:04:05 MST")
-	//conn.WriteToUDP([]byte(time), addr)
-
-	//file.Close()
 }
 
 func handleHelloRequest(conn *net.UDPConn, addr *net.UDPAddr, buffer []byte) {
